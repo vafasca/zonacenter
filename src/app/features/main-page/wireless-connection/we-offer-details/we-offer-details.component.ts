519eba85fd07be5dcf2d5474639f71eee54e6ae2
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-11-22 16:10:28 -0400

    added offer component responsive

--- src/app/features/main-page/wireless-connection/we-offer-details/we-offer-details.component.ts
+++ src/app/features/main-page/wireless-connection/we-offer-details/we-offer-details.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./we-offer-details.component.css']
 })
 export class WeOfferDetailsComponent {
-
+  featuresData = [
+    {
+      icon: 'icon1', 
+      title: 'Prueba nuestro servicio', 
+      description: 'Se le otorga un periodo de prueba del servicio con la certeza de que será de su agrado, caso contrario puede no aceptar el servicio sin problemas.'
+    },
+    {
+      icon: 'icon2',
+      title: 'Sin contratos forzosos',
+      description: 'No hay un contrato de permanencia obligatoria. Puedes estar con nuestro servicio el tiempo que desees.'
+    },
+    {
+      icon: 'icon3',
+      title: 'Facilidades de pago',
+      description: 'Puedes pagar directamente en nuestro local comercial, pagar por transferencia bancaria o por código QR desde la comodidad de tu casa.'
+    },
+    {
+      icon: 'icon4',
+      title: 'Beneficios a clientes',
+      description: 'Descuentos, atención prioritaria, tratamiento VIP y otros beneficios que faciliten tu integración digital. Accede a estos beneficios desde nuestro local comercial.'
+    }
+  ];
 }
