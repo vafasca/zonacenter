560693cc55de1502f7ffb9ae0cb98d46bd905f93
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-11-25 18:07:10 -0400

    added new style responsive to header component

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  ngOnInit(): void {
+
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
 }
