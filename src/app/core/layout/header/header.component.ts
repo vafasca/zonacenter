bb984b2be62b5bd9c1ee4b27e1c4b0c90c42f764
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2025-01-13 11:33:41 -0400

    updated view header component

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   menuOpen = false;
+  showExtraButtons = false;
 
   ngOnInit(): void {
-
+    this.checkScreenSize(); // Verificar el tamaño al cargar
   }
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    const screenWidth = window.innerWidth;
+    this.showExtraButtons = screenWidth <= 1250; // Mostrar botones extras en dropdown si el ancho es menor o igual a 1250px
   }
 }
